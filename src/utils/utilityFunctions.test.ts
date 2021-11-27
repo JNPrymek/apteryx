022fd7762f6f59ea0fc85db2f78832bfabf7b5f1
Jacob N Prymek <jacob@jnprymek.com>
2021-11-27 15:23:32 -0600

    Add test for timeUtil function

--- src/utils/utilityFunctions.test.ts
+++ src/utils/utilityFunctions.test.ts
@@ -1,4 +1,5 @@
 import prettyPrintJson from './prettyPrintJson';
+import TimeUtils from './timeUtils';
 
 describe('Utility Functions', () => {
 	
@@ -13,4 +14,20 @@ describe('Utility Functions', () => {
 			expect(console.log).toHaveBeenCalledWith(prettyPrintResults);
 		});
 	
+	describe('Time Utils', () => {
+		
+		it('Can interpret Kiwi Server date strings', () => {
+			// First millisecond of year 2021
+			const begin2021String = '2021-01-01T00:00:00.000';
+			const begin2021Val = 1609459200000;
+			const begin2021Date = TimeUtils.serverStringToDate(begin2021String);
+			expect(begin2021Date.valueOf()).toEqual(begin2021Val);
+			
+			// Random (realistic) date-time
+			const realisticString = '2021-10-24T03:37:59.158';
+			const realisticVal = 1635046679158;
+			const realisticDate = TimeUtils.serverStringToDate(realisticString);
+			expect(realisticDate.valueOf()).toEqual(realisticVal);
+		});
+	});
 });
