e2f1c12e8441791e81075d35111d90d80decaef1
Jacob Prymek <jacob@jnprymek.com>
2022-07-07 13:09:36 -0500

    Implement test case status class

---
+++ src/testCases/testCaseStatus.ts
@@ -0,0 +1,59 @@
+/*
+id: 1
+name: PROPOSED
+description: ''
+is_confirmed: false
+*/
+
+import KiwiNamedItem from '../core/kiwiNamedItem';
+
+export default class TestCaseStatus extends KiwiNamedItem {
+	// Constructor for all classes
+	constructor(serializedValues: Record<string, unknown>) {
+		super(serializedValues);
+	}
+
+	public isConfirmed(): boolean {
+		return this.serialized['is_confirmed'] as boolean;
+	}
+
+	public isRunnable(): boolean {
+		return this.isConfirmed();
+	}
+
+	public getDescription(): string {
+		return this.serialized['description'] as string;
+	}
+
+	// Inherited methods
+	// ------------------------------------------------------------------------
+	
+	// Kiwi Named
+	// --------------------------------
+	
+	public static async getByName(
+		name: string
+	): Promise<TestCaseStatus> {
+		return await super.getByName(name) as TestCaseStatus;
+	}
+	
+	public static async serverFilter(
+		filterObj: Record<string, unknown>
+	): Promise<Array<TestCaseStatus>> {
+		return await super.serverFilter(filterObj) as Array<TestCaseStatus>;
+	}
+	
+	public static async getByIds(
+		id: number | Array<number>
+	): Promise<Array<TestCaseStatus>> {
+		return await super.getByIds(id) as Array<TestCaseStatus>;
+	}
+	
+	public static async getById(
+		id: number
+	): Promise<TestCaseStatus> {
+		return await super.getById(id) as TestCaseStatus;
+	}
+	
+	// ------------------------------------------------------------------------
+}
