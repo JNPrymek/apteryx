4aa9bcb20a8d298d7a1925028e4962bdd28ece77
Jacob Prymek <jacob@jnprymek.com>
2022-12-19 21:01:45 -0600

    Implement tests for TestPlan getChildren and getPlansWithTestCase

--- src/testPlans/testPlan.test.ts
+++ src/testPlans/testPlan.test.ts
@@ -50,8 +50,46 @@ describe('Test Plan', () => {
 		parent: 1
 	};
 
+	const plan3Vals = {
+		id: 3,
+		name: 'Plan 3',
+		text: 'Another Another Example Test Plan',
+		create_date: '2022-12-02T20:11:13.015',
+		is_active: false,
+		extra_link: 'This is an extra link',
+		product_version: 2,
+		product_version__value: 'v1.3.2',
+		product: 2,
+		product__name: 'ExampleApp',
+		author: 3,
+		author__username: 'joe',
+		type: 4,
+		type__name: 'Regression',
+		parent: 1
+	};
+
+	const plan4Vals = {
+		id: 4,
+		name: 'Plan 4',
+		text: 'Nested Test Plans',
+		create_date: '2022-12-02T20:12:13.015',
+		is_active: false,
+		extra_link: null,
+		product_version: 2,
+		product_version__value: 'v1.3.2',
+		product: 2,
+		product__name: 'ExampleApp',
+		author: 3,
+		author__username: 'joe',
+		type: 4,
+		type__name: 'Regression',
+		parent: 3
+	};
+
 	const tp1 = new TestPlan(plan1Vals);
 	const tp2 = new TestPlan(plan2Vals);
+	const tp3 = new TestPlan(plan3Vals);
+	const tp4 = new TestPlan(plan4Vals);
 
 	it('Can instantiate a TestPlan', () => {
 		const tp = new TestPlan(plan1Vals);
@@ -355,7 +393,6 @@ describe('Test Plan', () => {
 		});
 
 		it('Can get list of TestCases - TC ID order', async ()=> {
-			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({result: sortKeysRawResponse}));
 			mockAxios.post.mockResolvedValue(mockRpcResponse({result: testCaseRawValues}));
 			const planCases = await tp1.getTestCases('TESTCASE_ID');
 			expect(planCases).toEqual(tcListIdSort);
@@ -366,6 +403,73 @@ describe('Test Plan', () => {
 			mockAxios.post.mockResolvedValue(mockRpcResponse({result: testCaseRawValues}));
 			const planCases = await tp1.getTestCases('SORTKEY');
 			expect(planCases).toEqual(tcListKeySort);
+		});
+
+		it('Can get all test plans containing a specific test case - by ID', async () => {
+			mockAxios.post.mockResolvedValue(mockRpcResponse({result: [plan1Vals, plan3Vals]}));
+			const tc1Plans = await TestPlan.getPlansWithTestCase(1);
+			expect(tc1Plans).toEqual(expect.arrayContaining([tp1, tp3]));
+			expect(tc1Plans.length).toEqual(2);
+		});
+
+		it('Can get all test plans containing a specific test case - by TestCase', async () => {
+			mockAxios.post.mockResolvedValue(mockRpcResponse({result: [plan1Vals, plan3Vals]}));
+			const tc1Plans = await TestPlan.getPlansWithTestCase(tcListIdSort[0]);
+			expect(tc1Plans).toEqual(expect.arrayContaining([tp1, tp3]));
+			expect(tc1Plans.length).toEqual(2);
+		});
+	});
+
+	describe('TestPlan - TestPlan Relations', () => {
+		it('TestPlan with children can return list of direct child TestPlans', async () => {
+			mockAxios.post.mockResolvedValue(mockRpcResponse({result: [plan2Vals, plan3Vals]}));
+			const results = await tp1.getDirectChildren();
+			expect(Array.isArray(results)).toEqual(true);
+			expect(results.length).toEqual(2);
+			expect(results).toEqual(expect.arrayContaining([tp2, tp3]));
+			expect(results).toEqual(expect.not.arrayContaining([tp1]));
+		});
+
+		it('Can check if TestPlan has children', async () => {
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({result: [plan2Vals, plan3Vals]}));
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({result: []}));
+			
+			const tp1Children = await tp1.hasChildren(); // 2 direct children
+			const tp2Children = await tp2.hasChildren(); // no children
+
+			expect(tp1Children).toEqual(true);
+			expect(tp2Children).toEqual(false);
+		});
+
+		it('Can get TestPlan children - direct only', async () => {
+			mockAxios.post.mockResolvedValue(mockRpcResponse({result: [plan2Vals, plan3Vals]}));
+			const results = await tp1.getChildren(true);
+			expect(Array.isArray(results)).toEqual(true);
+			expect(results.length).toEqual(2);
+			expect(results).toEqual(expect.arrayContaining([tp2, tp3]));
+			expect(results).toEqual(expect.not.arrayContaining([tp1, tp4]));
+		});
+
+		it('Can get TestPlan children - all nested children, explicit', async () => {
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({result: [plan2Vals, plan3Vals]}));
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({result: []}));
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({result: [plan4Vals]}));
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({result: []}));
+			const tp1Children = await tp1.getChildren(false);
+
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({result: [plan4Vals]}));
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({result: []}));
+			const tp3Children = await tp3.getChildren(false);
+
+			expect(Array.isArray(tp1Children)).toEqual(true);
+			expect(tp1Children.length).toEqual(3);
+			expect(tp1Children).toEqual(expect.arrayContaining([tp2, tp3, tp4]));
+			expect(tp1Children).toEqual(expect.not.arrayContaining([tp1]));
+
+			expect(Array.isArray(tp3Children)).toEqual(true);
+			expect(tp3Children.length).toEqual(1);
+			expect(tp3Children).toEqual(expect.arrayContaining([tp4]));
+			expect(tp3Children).toEqual(expect.not.arrayContaining([tp1, tp2, tp3]));
 		});
 	});
 });
