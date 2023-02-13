0a181cd5752bd4ce1250f19d6b2aba4486b59ba7
Jacob Prymek <jacob@jnprymek.com>
2023-02-12 19:36:55 -0600

    Add types for TestCase and TestExeuction raw values

---
+++ src/testRuns/testExecution.type.ts
@@ -0,0 +1,18 @@
+export type TestExecutionValues = {
+	id: number,
+	assignee: number | null
+	assignee__username: string | null,
+	tested_by: number | null,
+	tested_by__username: string | null,
+	case_text_version: number,
+	start_date: string | null,
+	stop_date: string | null,
+	sortkey: number,
+	run: number,
+	case: number,
+	case__summary: string,
+	build: number,
+	build__name: string,
+	status: number,
+	status__name: string
+}
