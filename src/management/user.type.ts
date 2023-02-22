e30da9a332693b1171dfe6444f5f2d0ea8e435f7
Jacob Prymek <jacob@jnprymek.com>
2023-02-22 17:45:50 -0600

    Implement User object and server value types

---
+++ src/management/user.type.ts
@@ -0,0 +1,10 @@
+export type UserValues = {
+	id: number,
+	email: string,
+	username: string,
+	first_name: string,
+	last_name: string,
+	is_active: boolean,
+	is_staff: boolean,
+	is_superuser: boolean
+}
