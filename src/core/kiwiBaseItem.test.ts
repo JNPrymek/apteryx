5016545dd3f5270844c5d12847a474a28e0981d7
Jacob Prymek <jacob@jnprymek.com>
2023-03-06 16:45:13 -0600

    refactor KiwiBase files

--- src/core/kiwiBaseItem.test.ts
+++ src/core/kiwiBaseItem.test.ts
@@ -16,47 +16,59 @@ describe('KiwiBaseItem', () => {
 	KiwiConnector.init({ hostName: serverDomain });
 	
 	it('Can instantiate a KiwiBaseItem', () => {
-		const initVals = {id: 1, a: 'valA', b: 'valB'};
+		const initVals = { id: 1, a: 'valA', b: 'valB' };
 		const kbi = new KiwiBaseItem(initVals);
 		
 		expect(kbi['serialized']).toEqual(initVals);
 	});
 	
 	it('getId() returns the item ID', () => {
-		const kbi = new KiwiBaseItem({id: 123, otherKey: 'otherVal'});
+		const kbi = new KiwiBaseItem({ id: 123, otherKey: 'otherVal' });
 		expect(kbi.getId()).toEqual(123);
 	});
 	
 	it('Can get a single KiwiBaseItem by ID', async () => {
-		const serverKbis = [{ id: 1, otherKey: 'otherVal'}];
-		mockAxios.post.mockResolvedValue(mockRpcResponse({result: serverKbis}));
+		const serverKbis = [{ id: 1, otherKey: 'otherVal' }];
+		mockAxios.post.mockResolvedValue(
+			mockRpcResponse({ result: serverKbis })
+		);
 		const kbi = await KiwiBaseItem.getById(1);
 		expect(kbi['serialized']).toEqual(serverKbis[0]);
 	});
 	
 	it('Can get multiple KiwiBaseItems by ID array', async () => {
-		const serverKbis = [{ id: 1, otherKey: 'otherVal'}, { id: 2, otherKey: 'otherOtherVal'}];
-		mockAxios.post.mockResolvedValue(mockRpcResponse({result: serverKbis}));
+		const serverKbis = [
+			{ id: 1, otherKey: 'otherVal' }, 
+			{ id: 2, otherKey: 'otherOtherVal' }
+		];
+		mockAxios.post.mockResolvedValue(
+			mockRpcResponse({ result: serverKbis })
+		);
 		const kbi = await KiwiBaseItem.getByIds([1, 2]);
 		expectArrayWithKiwiItem(kbi, serverKbis[0]);
 		expectArrayWithKiwiItem(kbi, serverKbis[1]);
 	});
 	
 	it('GetByIds can handle int', async () => {
-		const serverKbis = [{ id: 1, otherKey: 'otherVal'}];
-		mockAxios.post.mockResolvedValue(mockRpcResponse({result: serverKbis}));
+		const serverKbis = [{ id: 1, otherKey: 'otherVal' }];
+		mockAxios.post.mockResolvedValue(
+			mockRpcResponse({ result: serverKbis })
+		);
 		const kbi = await KiwiBaseItem.getById(1);
 		expect(kbi['serialized']).toEqual(serverKbis[0]);
 	});
 	
 	it('Shows correct value for toString()', () => {
-		const kbi = new KiwiBaseItem({id: 123, otherKey: 'otherVal'});
+		const kbi = new KiwiBaseItem({ id: 123, otherKey: 'otherVal' });
 		const kbiString = kbi.toString();
-		expect(kbiString).toEqual('KiwiBaseItem:{"id":123,"otherKey":"otherVal"}');
+		expect(kbiString)
+			.toEqual('KiwiBaseItem:{"id":123,"otherKey":"otherVal"}');
 	});
 	
 	it('GetByID throws error with invalid ID', async ()=> {
-		mockAxios.post.mockResolvedValue(mockRpcResponse({result: []}));
-		expect(KiwiBaseItem.getById(1)).rejects.toThrowError('Could not find any KiwiBaseItem with ID 1');
+		mockAxios.post.mockResolvedValue(mockRpcResponse({ result: [] }));
+		expect(KiwiBaseItem.getById(1))
+			.rejects
+			.toThrowError('Could not find any KiwiBaseItem with ID 1');
 	});
 });
