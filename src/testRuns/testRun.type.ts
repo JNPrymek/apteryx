5e6c4ffd775cde617b77607e1c05f3f0eb70a8a5
Jacob Prymek <jacob@jnprymek.com>
2023-03-12 15:58:19 -0500

    Reformat TestRun Type file

--- src/testRuns/testRun.type.ts
+++ src/testRuns/testRun.type.ts
@@ -1,20 +1,20 @@
 export type TestRunValues = {
-	id: number,
-	plan__product_version: number,
-	plan__product_version__value: string,
-	start_date: string | null,
-	stop_date: string | null,
-	planned_start: string | null,
-	planned_stop: string | null,
-	summary: string,
-	notes: string,
-	plan: number,
-	plan__product: number,
-	plan__name: string,
-	build: number,
-	build__name: string,
-	manager: number,
-	manager__username: string,
-	default_tester: number | null,
-	default_tester__username: string | null
+	id: number;
+	plan__product_version: number;
+	plan__product_version__value: string;
+	start_date: string | null;
+	stop_date: string | null;
+	planned_start: string | null;
+	planned_stop: string | null;
+	summary: string;
+	notes: string;
+	plan: number;
+	plan__product: number;
+	plan__name: string;
+	build: number;
+	build__name: string;
+	manager: number;
+	manager__username: string;
+	default_tester: number | null;
+	default_tester__username: string | null;
 }
