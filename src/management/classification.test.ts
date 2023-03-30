1adc9f962b77cb50f81579e0d4f4bd87001aee67
Jacob Prymek <jacob@jnprymek.com>
2023-03-30 14:09:22 -0500

    Refactor Classification tests

--- src/management/classification.test.ts
+++ src/management/classification.test.ts
@@ -1,11 +1,8 @@
 import axios from 'axios';
-
-import KiwiConnector from '../core/kiwiConnector';
-
-import { serverDomain } from '../../test/testServerDetails';
 import mockRpcResponse from '../../test/axiosAssertions/mockRpcResponse';
 import Classification from './classification';
 import { ClassificationValues } from './classification.type';
+import { mockClassification } from '../../test/mockKiwiValues';
 
 // Mock Axios
 jest.mock('axios');
@@ -13,38 +10,58 @@ const mockAxios = axios as jest.Mocked<typeof axios>;
 
 describe('Classification', () => {
 	
-	KiwiConnector.init({ hostName: serverDomain });
-	
-	const serverClass1: ClassificationValues = {
-		id: 1, 
-		name: 'Class1' 
-	};
+	const class1Vals: ClassificationValues = mockClassification();
+	const class2Vals: ClassificationValues = mockClassification({ 
+		id: 2, 
+		name: 'Mobile App' 
+	});
 	
 	it('Can instantiate a Classification', () => {
-		const initVals = serverClass1;
-		const c1 = new Classification(initVals);
+		const class1 = new Classification(class1Vals);
+		const class2 = new Classification(class2Vals);
 		
-		expect(c1['serialized']).toEqual(initVals);
+		expect(class1['serialized']).toEqual(class1Vals);
+		expect(class2['serialized']).toEqual(class2Vals);
 	});
-	
-	it('Can get Classification by ID', async () => {
-		const serverItems: Array<Record<string, unknown>> = [serverClass1];
-		mockAxios
-			.post
-			.mockResolvedValue(mockRpcResponse({ result: serverItems }));
-		
-		const class1 = await Classification.getById(1);
-		expect(class1['serialized']).toEqual(serverItems[0]);
+
+	describe('Local Properties', () => {
+		const class1 = new Classification(class1Vals);
+		const class2 = new Classification(class2Vals);
+
+		it('Can get Classification ID', () => {
+			expect(class1.getId()).toEqual(1);
+			expect(class2.getId()).toEqual(2);
+		});
+
+		it('Can get Classification Name', () => {
+			expect(class1.getName()).toEqual('Website');
+			expect(class2.getName()).toEqual('Mobile App');
+		});
 	});
-	
-	it('Can get Classification by Name', async () => {
-		const serverItems: Array<Record<string, unknown>> = [serverClass1];
-		mockAxios
-			.post
-			.mockResolvedValue(mockRpcResponse({ result: serverItems }));
+
+	describe('Server Lookups', () => {
+		it('Can get Classification by ID', async () => {
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse(
+				{ result: [class1Vals] }
+			));
+			mockAxios.post.mockResolvedValue(mockRpcResponse(
+				{ result: [class2Vals] }
+			));
+			
+			const class1 = await Classification.getById(1);
+			const class2 = await Classification.getById(2);
+			expect(class1['serialized']).toEqual(class1Vals);
+			expect(class2['serialized']).toEqual(class2Vals);
+		});
 		
-		const class1 = await Classification.getByName('Class1');
-		expect(class1['serialized']).toEqual(serverItems[0]);
+		it('Can get Classification by Name', async () => {
+			mockAxios
+				.post
+				.mockResolvedValue(mockRpcResponse({ result: [class1Vals] }));
+			
+			const class1 = await Classification.getByName('Website');
+			expect(class1['serialized']).toEqual(class1Vals);
+		});
 	});
 	
 });
