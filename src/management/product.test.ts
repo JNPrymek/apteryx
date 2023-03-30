38edced4b3cf36990a9eb6ca18e7b99d03884c25
Jacob Prymek <jacob@jnprymek.com>
2023-03-30 14:24:21 -0500

    Refactor Product tests

--- src/management/product.test.ts
+++ src/management/product.test.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-import KiwiConnector from '../core/kiwiConnector';
-
-import { serverDomain } from '../../test/testServerDetails';
 import mockRpcResponse from '../../test/axiosAssertions/mockRpcResponse';
 import Product from './product';
 import Classification from './classification';
+import { mockClassification, mockProduct } from '../../test/mockKiwiValues';
 
 // Mock Axios
 jest.mock('axios');
@@ -13,64 +11,84 @@ const mockAxios = axios as jest.Mocked<typeof axios>;
 
 describe('Product', () => {
 	
-	KiwiConnector.init({ hostName: serverDomain });
-	
-	const serverProd1 = { 
-		id: 1, 
-		name: 'Prod1', 
-		classification: 2, 
-		description: 'Example Product 1' 
-	};
+	const prod1Vals = mockProduct();
+	const prod2Vals = mockProduct({ 
+		id: 2, 
+		name: 'Example App', 
+		classification: 2,
+		description: 'An example mobile app'
+	});
 	
 	it('Can instantiate a Product', () => {
 		
-		const prod = new Product(serverProd1);
+		const prod = new Product(prod1Vals);
 		
-		expect(prod['serialized']).toEqual(serverProd1);
+		expect(prod['serialized']).toEqual(prod1Vals);
 	});
 	
 	describe('Access local properties', () => {
-		const prod1 = new Product(serverProd1);
+		const prod1 = new Product(prod1Vals);
+		const prod2 = new Product(prod2Vals);
 		
 		it('Can get Product description', () => {
-			expect(prod1.getDescription()).toEqual('Example Product 1');
+			expect(prod1.getDescription()).toEqual('Example Website');
+			expect(prod2.getDescription()).toEqual('An example mobile app');
 		});
 		
 		it('Can get ID of Products classification', () => {
-			expect(prod1.getClassificationId()).toEqual(2);
+			expect(prod1.getClassificationId()).toEqual(1);
+			expect(prod2.getClassificationId()).toEqual(2);
 		});
 		
 		it('Can get Classification that a Product falls under', async () => {
-			const class2Vals = { id: 2, name: 'Classification2' };
+			const class1Vals = mockClassification();
+			const class2Vals = mockClassification({ 
+				id: 2,
+				name: 'Mobile App'
+			});
 			
+			mockAxios.post.mockResolvedValueOnce(
+				mockRpcResponse({ result: [class1Vals] })
+			);
+
 			mockAxios.post.mockResolvedValue(
 				mockRpcResponse({ result: [class2Vals] })
 			);
 			
 			expect(await prod1.getClassification())
+				.toEqual(new Classification(class1Vals));
+			expect(await prod2.getClassification())
 				.toEqual(new Classification(class2Vals));
 		});
 	});
 	
 	describe('Fetch from server', () => {
 		it('Can get Product by ID', async () => {
-			const serverItems: Array<Record<string, unknown>> = [serverProd1];
-			mockAxios
-				.post
-				.mockResolvedValue(mockRpcResponse({ result: serverItems }));
-			
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({ 
+				result: [prod1Vals] 
+			}));
+			mockAxios.post.mockResolvedValue(mockRpcResponse({ 
+				result: [prod2Vals] 
+			}));
+
 			const prod1 = await Product.getById(1);
-			expect(prod1['serialized']).toEqual(serverItems[0]);
+			const prod2 = await Product.getById(2);
+			expect(prod1['serialized']).toEqual(prod1Vals);
+			expect(prod2['serialized']).toEqual(prod2Vals);
 		});
 		
 		it('Can get Product by Name', async () => {
-			const serverItems: Array<Record<string, unknown>> = [serverProd1];
-			mockAxios
-				.post
-				.mockResolvedValue(mockRpcResponse({ result: serverItems }));
+			mockAxios.post.mockResolvedValueOnce(mockRpcResponse({ 
+				result: [prod1Vals] 
+			}));
+			mockAxios.post.mockResolvedValue(mockRpcResponse({ 
+				result: [prod2Vals] 
+			}));
 			
-			const prod1 = await Product.getByName('Prod1');
-			expect(prod1['serialized']).toEqual(serverItems[0]);
+			const prod1 = await Product.getByName('Example.com Website');
+			const prod2 = await Product.getByName('Example App');
+			expect(prod1['serialized']).toEqual(prod1Vals);
+			expect(prod2['serialized']).toEqual(prod2Vals);
 		});
 	});
 	
