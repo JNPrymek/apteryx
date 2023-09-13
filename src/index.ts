2290ce0e268d1c0805dfc099132a52f7fb35c8b8
Jacob Prymek <jacob@jnprymek.com>
2023-09-12 23:02:52 -0500

    Configure TS compilation

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+// Core
+import KiwiConnector from './core/kiwiConnector';
+
+// Management
+import Build from './management/build';
+import Classification from './management/classification';
+import Component from './management/component';
+import Priority from './management/priority';
+import Product from './management/product';
+import Tag from './management/tag';
+import User from './management/user';
+
+// Environment
+import Environment from './environments/environment';
+import EnvironmentProperty from './environments/environmentProperty';
+
+// Test Cases
+import Category from './testCases/category';
+import TestCase from './testCases/testCase';
+import TestCaseProperty from './testCases/testCaseProperty';
+import TestCaseStatus from './testCases/testCaseStatus';
+
+// Test Plan
+import PlanType from './testPlans/planType';
+import TestPlan from './testPlans/testPlan';
+
+// Test Run
+import TestExecution from './testRuns/testExecution';
+import TestExecutionStatus from './testRuns/testExecutionStatus';
+import TestRun from './testRuns/testRun';
+
+export {
+	// Core
+	KiwiConnector,
+	// Management
+	Build,
+	Classification,
+	Component,
+	Priority,
+	Product,
+	Tag,
+	User,
+	// Environment
+	Environment,
+	EnvironmentProperty,
+	// Test Case
+	Category,
+	TestCase,
+	TestCaseProperty,
+	TestCaseStatus,
+	// Test Plan
+	PlanType,
+	TestPlan,
+	// Test Run
+	TestExecution,
+	TestExecutionStatus,
+	TestRun
+};
+
