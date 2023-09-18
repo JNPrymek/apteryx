0115404f36ed2efd15c86bd29fe34f5077fe535c
Jacob Prymek <jacob@jnprymek.com>
2023-09-17 19:24:33 -0500

    Move type exports into main index file

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,147 @@
 // Core
 import KiwiConnector from './core/kiwiConnector';
+import {
+	RpcResult,
+	RpcParam,
+	IServerDetails
+} from './core/networkTypes';
 
 // Management
 import Build from './management/build';
+import { BuildValues } from './management/build.type';
 import Classification from './management/classification';
+import { ClassificationValues } from './management/classification.type';
 import Component from './management/component';
+import {
+	ComponentServerValues,
+	ComponentValues
+} from './management/component.type';
 import Priority from './management/priority';
+import { PriorityValues } from './management/priority.type';
 import Product from './management/product';
+import { ProductValues } from './management/product.type';
 import Tag from './management/tag';
+import {
+	TagServerValues,
+	TagValues
+} from './management/tag.type';
 import User from './management/user';
+import { UserValues } from './management/user.type';
 import Version from './management/version';
+import { VersionValues } from './management/version.type';
+
 
 // Environment
 import Environment from './environments/environment';
+import {
+	EnvironmentValues,
+	EnvironmentWriteValues
+} from './environments/environment.type';
 import EnvironmentProperty from './environments/environmentProperty';
+import {
+	EnvironmentPropertyValues
+} from './environments/environmentProperty.type';
 
 // Test Cases
 import Category from './testCases/category';
+import { CategoryValues } from './testCases/category.type';
 import TestCase from './testCases/testCase';
+import {
+	TestCaseValues,
+	TestCaseCreateValues,
+	TestCaseWriteValues
+} from './testCases/testCase.type';
 import TestCaseProperty from './testCases/testCaseProperty';
+import { TestCasePropertyValues } from './testCases/testCaseProperty.type';
 import TestCaseStatus from './testCases/testCaseStatus';
+import { TestCaseStatusValues } from './testCases/testCaseStatus.type';
 
 // Test Plan
 import PlanType from './testPlans/planType';
+import { PlanTypeValues } from './testPlans/planType.type';
 import TestPlan from './testPlans/testPlan';
+import {
+	TestPlanValues,
+	TestPlanCreateValues,
+	TestPlanWriteValues
+} from './testPlans/testPlan.type';
 
 // Test Run
 import TestExecution from './testRuns/testExecution';
+import {
+	TestExecutionValues,
+	TestExecutionWriteValues,
+} from './testRuns/testExecution.type';
 import TestExecutionStatus from './testRuns/testExecutionStatus';
+import {
+	TestExecutionStatusValues
+} from './testRuns/testExecutionStatus.type';
 import TestRun from './testRuns/testRun';
+import {
+	TestRunValues,
+	TestRunWriteValues,
+	TestRunCreateValues
+} from './testRuns/testRun.type';
+
 
 export {
 	// Core
 	KiwiConnector,
+	RpcResult,
+	RpcParam,
+	IServerDetails,
 	// Management
 	Build,
+	BuildValues,
 	Classification,
+	ClassificationValues,
 	Component,
+	ComponentServerValues,
+	ComponentValues,
 	Priority,
+	PriorityValues,
 	Product,
+	ProductValues,
 	Tag,
+	TagServerValues,
+	TagValues,
 	User,
+	UserValues,
 	Version,
+	VersionValues,
 	// Environment
 	Environment,
+	EnvironmentValues,
+	EnvironmentWriteValues,
 	EnvironmentProperty,
+	EnvironmentPropertyValues,
 	// Test Case
 	Category,
+	CategoryValues,
 	TestCase,
+	TestCaseValues,
+	TestCaseCreateValues,
+	TestCaseWriteValues,
 	TestCaseProperty,
+	TestCasePropertyValues,
 	TestCaseStatus,
+	TestCaseStatusValues,
 	// Test Plan
 	PlanType,
+	PlanTypeValues,
 	TestPlan,
+	TestPlanValues,
+	TestPlanCreateValues,
+	TestPlanWriteValues,
 	// Test Run
 	TestExecution,
+	TestExecutionValues,
+	TestExecutionWriteValues,
 	TestExecutionStatus,
-	TestRun
+	TestExecutionStatusValues,
+	TestRun,
+	TestRunValues,
+	TestRunWriteValues,
+	TestRunCreateValues,
 };
 
